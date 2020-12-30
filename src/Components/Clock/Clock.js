d34a4951332daee7d4f5928d9a040899a3d8973b
Daniel Lazar <lazardani0112@gmail.com>
2020-12-30 22:51:50 +0100

    First commit

---
+++ src/Components/Clock/Clock.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './Clock.css';
+
+const Clock = ({ forEverYear }) => {
+
+	const [value, setValue] = useState();
+ 
+	useEffect(() => {
+		const interval = setInterval(
+			() => {
+            const today = new Date();
+            let date = today.toLocaleDateString('hu-HU');
+            const time = today.toLocaleTimeString('hu-HU');
+            // console.log(today.getFullYear());
+            if (today.getFullYear() > forEverYear) {
+               date = `${forEverYear}. ${today.getMonth() + (12 * (today.getFullYear() - forEverYear))}. ${today.getDate()}.`;
+            }
+            setValue(`${date} ${time}`);
+
+         },
+			1000
+		);
+
+		return () => {
+			clearInterval(interval);
+		}
+	}, []);
+
+   useEffect(() => {
+      const dateTimeClassList = document.querySelector('.date-time').classList;
+      if (value && !dateTimeClassList.contains('fadedIn')) {
+         dateTimeClassList.add('fadedIn');
+      }
+   }, [value]);
+
+   return(
+      <div className='Clock'>
+         <h1 className='date-time'>{value}</h1>
+      </div>
+   );
+}
+
+export default Clock;
